24bdbfe72189e057ccfb26e5752ed90805247dea
Aparna Mohan <55956294+Aparnamohan1312@users.noreply.github.com>
2024-02-11 19:58:36 -0800

    Deleted

--- Views.py.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import generics
-from .models import Item
-from .serializers import ItemSerializer
-
-class ItemListAPIView(generics.ListAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
